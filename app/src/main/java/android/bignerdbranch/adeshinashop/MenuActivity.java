37dfb17531610959ab26862f07fe6bcd921581a3
stiger <adeshina.shakiru@yahoo.com>
2019-09-19 23:09:24 -0400

    checkOut Done!

--- app/src/main/java/android/bignerdbranch/adeshinashop/MenuActivity.java
+++ app/src/main/java/android/bignerdbranch/adeshinashop/MenuActivity.java
@@ -36,6 +36,10 @@ public class MenuActivity extends AppCompatActivity {
         mCurrentTotal = findViewById(R.id.current_amount);
     }
 
+    /**
+     * Method to increase the quantity
+     * @param view increase button
+     */
     public void quantityIncrease(View view) {
         TextView quantityView = getQuantityView(view);
         int mCount = Integer.parseInt(quantityView.getText().toString());
@@ -52,6 +56,10 @@ public class MenuActivity extends AppCompatActivity {
         subTotalTextView.setText(String.format("%.2f", Float.parseFloat(subTotalTextView.getText().toString()) + mPrice));
     }
 
+    /**
+     * Method to decrease the quantity of items
+     * @param view decrease button
+     */
     public void quantityDecrease(View view) {
         TextView quantityView = getQuantityView(view);
         int mCount = Integer.parseInt(quantityView.getText().toString());
@@ -70,6 +78,10 @@ public class MenuActivity extends AppCompatActivity {
         quantityView.setText(Integer.toString(mCount));
     }
 
+    /**
+     * Method to add items to cart
+     * @param view addToCart Button
+     */
     public void addToCart(View view) {
         ViewGroup motherView = (ViewGroup)view.getParent().getParent();
         TextView quantity = (TextView)((ViewGroup)motherView.getChildAt(3)).getChildAt(1);
@@ -86,22 +98,41 @@ public class MenuActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Method to get the price of an item
+     * @param viewGroup the viewGroup that contains the item
+     * @return item price
+     */
     public float getItemPrice(ViewGroup viewGroup){
         ViewGroup priceParent = (ViewGroup) ((ViewGroup)viewGroup.getParent()).getChildAt(2);
         TextView priceView = (TextView) priceParent.getChildAt(0);
         return Float.parseFloat(priceView.getText().toString());
     }
 
+    /**
+     * Method to get the subtotal TextView
+     * @param viewGroup The viewGroup that contains the subtotal TextView
+     * @return subTotal TextView
+     */
     public TextView getSubtotalTextView(ViewGroup viewGroup){
         ViewGroup subToTalParent = (ViewGroup)((ViewGroup)viewGroup.getParent()).getChildAt(5);
         return (TextView) subToTalParent.getChildAt(1);
 
     }
 
+    /**
+     * Method to get the previous quantity of the item selected for updating
+     * @param view the increase/Decrease button view
+     * @return quantity TextView
+     */
     public TextView getQuantityView(View view){
         return (TextView)((ViewGroup)view.getParent()).getChildAt(1);
     }
 
+    /**
+     * Checkout Method launches the checkout activity and send the total amount through an intent
+     * @param view checkout textView
+     */
     public void checkout(View view) {
         if (mNoOfItems != 0) {
             Intent intent = new Intent(this, CheckoutActivity.class);
